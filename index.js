a0879f49653a00210ddf83da31500ec48f15cade
Anna Schmidt <marshall1697@gmail.com>
2022-08-30 18:42:24 -0600

    commit tree 6b170afbe53baa9e7bf7016df7a7a2c176eb96d8

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+const destructivelyAppendCat = function(){
+    
+    return cats.push('Ralph')
+
+};
+
+const destructivelyPrependCat = function(){
+    return cats.unshift('Bob');
+}
+
+const destructivelyRemoveLastCat = function(){
+    return cats.pop();
+}
+
+const destructivelyRemoveFirstCat = function(){
+    return cats.shift();
+}
+
+const appendCat = function(){
+    const newCats = [...cats, 'Broom'];
+    return newCats;
+}
+
+const prependCat = function(){
+    const newCats = ['Arnold', ...cats];
+    return newCats;
+}
+
+const removeLastCat = function(){
+    return cats.slice(0,2)
+}
+const removeFirstCat = function(){
+    return cats.slice(1, cats.length);
+}
